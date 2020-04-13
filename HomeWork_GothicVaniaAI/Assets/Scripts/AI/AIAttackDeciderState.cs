829039ce15efd706c8ed52c3fd53ef8cde97a3a3
AstrexXx <astrex17@abv.bg>
2020-04-13 19:05:39 +0300

    Last tweeks

--- HomeWork_GothicVaniaAI/Assets/Scripts/AI/AIAttackDeciderState.cs
+++ HomeWork_GothicVaniaAI/Assets/Scripts/AI/AIAttackDeciderState.cs
@@ -3,7 +3,7 @@
 public class AIAttackDeciderState : StateMachineBehaviour {
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		float rand = Random.value;
-		if      (rand <= 0.5f) { animator.SetTrigger("ShouldJump"); }
+		if      (rand <= 0.3f) { animator.SetTrigger("ShouldJump"); }
 		else if (rand <= 1.0f) { animator.SetTrigger("ShouldMoveToPlayer"); }
 	}
 }
